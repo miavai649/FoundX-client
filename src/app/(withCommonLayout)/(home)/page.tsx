1fa6f15a0462131f97ffd63909978d75da677dc8
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2024-09-18 10:41:28 +0600

    register page created and create a group layout for home

--- src/app/(withCommonLayout)/(home)/page.tsx
+++ src/app/(withCommonLayout)/(home)/page.tsx
@@ -1,6 +1,5 @@
 import Landing from '@/src/components/modules/home/Landing'
 import RecentPost from '@/src/components/modules/home/RecentPost'
-import { Suspense } from 'react'
 
 export default function Home() {
   return (
