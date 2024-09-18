fed89f8b6f5630cef77cb58c0ef4275f89a4513a
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2024-09-18 12:14:51 +0600

    card skeleton created

--- src/app/(withCommonLayout)/(home)/@recentPosts/loading.tsx
+++ src/app/(withCommonLayout)/(home)/@recentPosts/loading.tsx
@@ -1,8 +1,26 @@
+import CardSkeleton from '@/src/components/UI/CardSkeleton'
+import Container from '@/src/components/UI/Container'
+import { Button } from '@nextui-org/button'
+import Link from 'next/link'
+
 const loading = () => {
   return (
-    <div>
-      <h1 className='p-20'>Loading</h1>
-    </div>
+    <Container>
+      <div className='my-8 section-title'>
+        <h2 className='text-2xl text-center mb-2'>Recently Found Items</h2>
+        <p className='text-center'>
+          A list of items that have been recently found and reported
+        </p>
+      </div>
+      <div className='grid my-8 sm:grid-cols-1 md:grid-cols-4 gap-10 justify-center'>
+        {[...Array(9)]?.map(() => <CardSkeleton />)}
+      </div>
+      <div className='flex justify-center'>
+        <Button className='rounded-md text-default bg-default-900' size='md'>
+          <Link href={'/found-items'}>See All</Link>
+        </Button>
+      </div>
+    </Container>
   )
 }
 
