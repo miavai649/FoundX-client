de8b7d625609b7ad0f422d9de60d9b2b3780fc21
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2024-09-22 15:17:52 +0600

    loading component created and addded into login page

--- src/app/(withCommonLayout)/login/page.tsx
+++ src/app/(withCommonLayout)/login/page.tsx
@@ -6,39 +6,46 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import Link from 'next/link'
 import { FieldValues, SubmitHandler } from 'react-hook-form'
 import loginValidationSchema from '@/src/schemas/login.schema'
+import { useUserLogin } from '@/src/hooks/auth.hook'
+import Loading from '@/src/components/UI/Loading'
 
 const page = () => {
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data)
+  const { mutate: handleUserLogin, isPending } = useUserLogin()
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    await handleUserLogin(data)
   }
 
   return (
-    <div className='flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center'>
-      <h3 className='my-2 text-2xl font-bold'>Login with FoundX</h3>
-      <p className='mb-4'>Welcome Back! Let&lsquo;s Get Started</p>
-      <div className='w-[35%]'>
-        <FXForm
-          onSubmit={onSubmit}
-          resolver={zodResolver(loginValidationSchema)}>
-          <div className='py-3'>
-            <FXInput name='email' label='Email' type='email' />
-          </div>
-          <div className='py-3'>
-            <FXInput name='password' label='Password' type='password' />
-          </div>
+    <>
+      {isPending && <Loading />}
+      <div className='flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center'>
+        <h3 className='my-2 text-2xl font-bold'>Login with FoundX</h3>
+        <p className='mb-4'>Welcome Back! Let&lsquo;s Get Started</p>
+        <div className='w-[35%]'>
+          <FXForm
+            onSubmit={onSubmit}
+            resolver={zodResolver(loginValidationSchema)}>
+            <div className='py-3'>
+              <FXInput name='email' label='Email' type='email' />
+            </div>
+            <div className='py-3'>
+              <FXInput name='password' label='Password' type='password' />
+            </div>
 
-          <Button
-            className='my-3 w-full rounded-md bg-default-900 font-semibold text-default'
-            size='lg'
-            type='submit'>
-            Login
-          </Button>
-        </FXForm>
-        <div className='text-center'>
-          Don&lsquo;t have account ? <Link href={'/register'}>Register</Link>
+            <Button
+              className='my-3 w-full rounded-md bg-default-900 font-semibold text-default'
+              size='lg'
+              type='submit'>
+              Login
+            </Button>
+          </FXForm>
+          <div className='text-center'>
+            Don&lsquo;t have account ? <Link href={'/register'}>Register</Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
