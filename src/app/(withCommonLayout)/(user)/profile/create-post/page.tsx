d17387ed24e4f0cf49e1172feb049781b273cee6
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2024-09-24 14:31:25 +0600

    create post form created

--- src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
@@ -37,26 +37,66 @@ const page = () => {
   }
 
   return (
-    <div>
+    <div className='h-full rounded-xl bg-gradient-to-b from-default-100 px-[73px] py-12'>
+      <h1 className='text-2xl font-semibold'>Post a found item</h1>
+      <Divider className='mb-5 mt-3' />
       <FormProvider {...methods}>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <FXInput name='title' label='Title' />
-          <Divider className='my-3' />
-          <div className='flex justify-between items-center'>
+          <div className='flex flex-wrap gap-2 py-2'>
+            <div className='min-w-fit flex-1'>
+              <FXInput label='Title' name='title' />
+            </div>
+            <div className='min-w-fit flex-1'>
+              <FXInput label='Found date' name='dateFound' />
+            </div>
+          </div>
+          <div className='flex flex-wrap gap-2 py-2'>
+            <div className='min-w-fit flex-1'>
+              <FXInput label='Location' name='location' />
+            </div>
+            <div className='min-w-fit flex-1'>
+              <FXInput label='City' name='city' />
+            </div>
+          </div>
+          <div className='flex flex-wrap gap-2 py-2'>
+            <div className='min-w-fit flex-1'>
+              <FXInput label='Category' name='category' />
+            </div>
+            <div className='min-w-fit flex-1'>
+              <FXInput label='Upload image' name='category' />
+            </div>
+          </div>
+
+          <div className='flex flex-wrap-reverse gap-2 py-2'>
+            <div className='min-w-fit flex-1'>
+              <FXInput label='Description' name='description' />
+            </div>
+          </div>
+
+          <Divider className='my-5' />
+
+          <div className='flex justify-between items-center mb-5'>
             <h1 className='text-xl'>Owner verification questions</h1>
             <Button onClick={() => handleFieldAppend()}>Append</Button>
           </div>
 
-          {fields.map((field, index) => (
-            <div key={field.id} className='flex items-center gap-2'>
-              <FXInput name={`questions.${index}.value`} label='Question' />
-              <Button onClick={() => remove(index)}>Remove</Button>
-            </div>
-          ))}
+          <div className='space-y-5'>
+            {fields.map((field, index) => (
+              <div key={field.id} className='flex gap-2 items-center'>
+                <FXInput label='Question' name={`questions.${index}.value`} />
+                <Button className='h-14 w-16' onClick={() => remove(index)}>
+                  Remove
+                </Button>
+              </div>
+            ))}
+          </div>
 
-          <Divider className='my-3' />
-
-          <Button type='submit'>Post</Button>
+          <Divider className='my-5' />
+          <div className='flex justify-end'>
+            <Button size='lg' type='submit'>
+              Post
+            </Button>
+          </div>
         </form>
       </FormProvider>
     </div>
