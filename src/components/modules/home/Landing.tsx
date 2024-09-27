6e94b0776e168f22fdb0ca53bf53d4bb0a949b80
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2024-09-27 23:51:55 +0600

    debounce hook created

--- src/components/modules/home/Landing.tsx
+++ src/components/modules/home/Landing.tsx
@@ -1,13 +1,25 @@
+'use client'
 import { Input } from '@nextui-org/input'
 import { SearchIcon } from '../../../assets/icons'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import { useDebounce } from '@/src/hooks/debounce.hook'
 
 const Landing = () => {
+  const { register, handleSubmit, watch } = useForm()
+
+  console.log(useDebounce(watch('search')))
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data)
+  }
+
   return (
     <div className="h-[calc(100vh-64px)] bg-[url('/glass.jpg')] bg-cover bg-center">
       <div className='max-w-xl mx-auto pt-32 flex-1'>
-        <form className='flex-1'>
+        <form onSubmit={handleSubmit(onSubmit)} className='flex-1'>
           <Input
             aria-label='Search'
+            {...register('search')}
             classNames={{
               innerWrapper: 'bg-default-100',
               input: 'text-sm'
